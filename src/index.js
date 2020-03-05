ce20a29e7f504e0986776ab928f393e784b95fa1
Markus Heck <markus.heck13396@gmx.de>
2020-03-05 01:27:37 +0100

    add first version with get/getAll, all/one and request interceptors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,86 @@
-import React, { Component } from 'react';
-
-export default class DummyComponent extends Component {
-
-    render () {
-
-        return (
-            <div>I am a dummy react npm module</div>
-        )
-
+export default class Api {
+    constructor(baseUrl, headers = {}, subUrl = '', requestInterceptors = []) {
+        this.baseUrl = baseUrl;
+        this.subUrl = subUrl;
+        this.requestInterceptors = requestInterceptors;
+        this.headers = headers;
     }
 
+    addRequestInterceptor(interceptor) {
+        this.requestInterceptors.push(interceptor);
+    }
+
+    async get(id) {
+        this.subUrl += '/' + id;
+        return await this._get();
+    }
+
+    async getAll() {
+        return await this._get();
+    }
+
+    async _get() {
+        let headers = this.headers;
+        let url = this.baseUrl + this.subUrl;
+        let method = 'GET';
+
+        this.requestInterceptors.forEach(interceptor => {
+            const options = interceptor({
+                data: {}, // not yet supported, just for compatibility reasons
+                params: {}, // not yet supported, just for compatibility reasons
+                headers: headers,
+                url: url,
+                method: method
+            });
+            headers = options.headers ?? headers;
+            url = options.url ?? url;
+            method = options.method ?? method;
+        });
+
+        let response = await fetch(url, {
+            method: method,
+            headers: new Headers(headers),
+        });
+
+        return {
+            body: await response.json(),
+            status: response.status
+        };
+    }
+
+    all(name) {
+        const newSubUrl = this.subUrl + this._normalizeSubUrl(name);
+        return new Api(this.baseUrl, this.headers, newSubUrl, this.requestInterceptors);
+    }
+
+    one(name, id) {
+        const newSubUrl = this.subUrl + this._normalizeSubUrl(name) + '/' + id;
+        return new Api(this.baseUrl, this.headers, newSubUrl, this.requestInterceptors);
+    }
+
+    post() {
+    }
+
+    put() {
+    }
+
+    delete() {
+    }
+
+    /// ensures urlPart has a leading /
+    _normalizeSubUrl(urlPart) {
+        if(urlPart[0] === '/')
+            return urlPart;
+        return '/' + urlPart;
+    }
+
+    set baseUrl(val) {
+        if (val.slice(-1) === '/')
+            this._baseUrl = val.slice(0, -1);
+        this._baseUrl = val;
+    }
+
+    get baseUrl() {
+        return this._baseUrl;
+    }
 }
