a18312c6af8bd657009cde082766c1f13e4ce989
Markus Heck <markus.heck13396@gmx.de>
2020-03-05 22:25:42 +0100

    reafactor code and add post put patch and delete

--- src/index.js
+++ src/index.js
@@ -11,41 +11,55 @@ export default class Api {
     }
 
     async get(id) {
-        this.subUrl += '/' + id;
-        return await this._get();
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl + '/' + id,
+            method: 'GET'
+        });
     }
 
     async getAll() {
-        return await this._get();
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl,
+            method: 'GET'
+        });
     }
 
-    async _get() {
-        let headers = this.headers;
-        let url = this.baseUrl + this.subUrl;
-        let method = 'GET';
-
-        this.requestInterceptors.forEach(interceptor => {
-            const options = interceptor({
-                data: {}, // not yet supported, just for compatibility reasons
-                params: {}, // not yet supported, just for compatibility reasons
-                headers: headers,
-                url: url,
-                method: method
-            });
-            headers = options.headers ?? headers;
-            url = options.url ?? url;
-            method = options.method ?? method;
+    async post(data = {}) {
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl,
+            method: 'POST',
+            data: data
         });
+    }
 
-        let response = await fetch(url, {
-            method: method,
-            headers: new Headers(headers),
+    async put(id, data = {}) {
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl + '/' + id,
+            method: 'PUT',
+            data: data
         });
+    }
 
-        return {
-            body: await response.json(),
-            status: response.status
-        };
+    async patch(id, data = {}) {
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl + '/' + id,
+            method: 'PATCH',
+            data: data
+        });
+    }
+
+    async delete(id, data = {}) {
+        return await Api._doRequest({
+            headers: this.headers,
+            url: this.baseUrl + this.subUrl + '/' + id,
+            method: 'DELETE',
+            data: data
+        });
     }
 
     all(name) {
@@ -58,22 +72,6 @@ export default class Api {
         return new Api(this.baseUrl, this.headers, newSubUrl, this.requestInterceptors);
     }
 
-    post() {
-    }
-
-    put() {
-    }
-
-    delete() {
-    }
-
-    /// ensures urlPart has a leading /
-    _normalizeSubUrl(urlPart) {
-        if(urlPart[0] === '/')
-            return urlPart;
-        return '/' + urlPart;
-    }
-
     set baseUrl(val) {
         if (val.slice(-1) === '/')
             this._baseUrl = val.slice(0, -1);
@@ -82,5 +80,68 @@ export default class Api {
 
     get baseUrl() {
         return this._baseUrl;
+    }
+
+    async static _doRequest({url, method, headers, body, params}) {
+        const riResult = Api._doRequestInterceptors({
+            headers: headers,
+            url: url,
+            method: method,
+            data: body,
+            params: params
+        });
+
+        let response = await fetch(riResult.url, {
+            method: riResult.method,
+            headers: new Headers(riResult.headers),
+            body: riResult.data,
+        });
+
+        // TODO response interceptor
+        // TODO error interceptor
+
+        // TODO more fields for response
+        return {
+            body: await response.json(),
+            status: response.status
+        };
+    }
+
+    static _doRequestInterceptors({headers, url, method, data, params}) {
+        let _headers = headers;
+        let _url = url;
+        let _method = method;
+        let _data = data;
+        let _params = params;
+
+        this.requestInterceptors.forEach(interceptor => {
+            const options = interceptor({
+                data: _data,
+                params: _params,
+                headers: _headers,
+                url: _url,
+                method: _method
+            });
+            _headers = options.headers ?? _headers;
+            _url = options.url ?? _url;
+            _method = options.method ?? _method;
+            _data = options.data ?? _data;
+            _params = options.params ?? _params;
+        });
+
+        return {
+            headers: _headers,
+            url: _url,
+            method: _method,
+            data: _data,
+            params: _params,
+        }
+    }
+
+    /// ensures urlPart has a leading /
+    _normalizeSubUrl(urlPart) {
+        if (urlPart[0] === '/')
+            return urlPart;
+        return '/' + urlPart;
     }
 }
